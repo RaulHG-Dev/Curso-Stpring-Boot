60a604ca8ba879da7da9933bd0df1350f88d35db
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2026-05-01 12:13:53 -0600

    Anotación @ModelAttribute

--- src/main/java/com/raul/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/raul/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.raul.curso.springboot.webapp.springboot_web.controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import models.User;
 
@@ -36,11 +38,18 @@ public class UserController {
     public String list(
         ModelMap model
     ) {
-        List<User> users = new ArrayList<>();
-
-        model.addAttribute("users", users);
+        // model.addAttribute("users", users);
         model.addAttribute("title", "Listado de usuarios");
         return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel() {
+        return Arrays.asList(
+            new User("Raul", "Hernandez"),
+            new User("Maria", "Gomez", "maria.gomez@example.com"), 
+            new User("Juan", "Perez")
+        );
     }
     
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -5,6 +5,10 @@ public class User {
     private String lastname;
     private String email;
 
+    public User(String name, String lastname, String email) {
+        this(name, lastname);
+        this.email = email;
+    }
 
     public User(String name, String lastname) {
         this.name = name;
